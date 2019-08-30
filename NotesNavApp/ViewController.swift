120e8783eba3ba78c37618fbcee0e52c3bad3489
MichaelWTA <michael.pollock@willowtreeapps.com>
2019-08-30 08:25:18 -0400

    Initial Commit

---
+++ NotesNavApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotesNavApp
+//
+//  Created by Michael P on 8/30/19.
+//  Copyright © 2019 Michael P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
